0239cc3f4e86f7031c7323ae5cf4f36518ab81e3
Matthew Dilley <matthewrdilley@gmail.com>
2023-12-13 11:42:22 -0500

    fixed window title not working

--- app/src/main/kotlin/graphics/Screen.kt
+++ app/src/main/kotlin/graphics/Screen.kt
@@ -5,12 +5,13 @@ import javax.swing.JFrame
 
 class Screen internal constructor(
     private val runtimeSettings: RuntimeSettings
-) : JFrame(runtimeSettings.windowTitle) {
+) : JFrame() {
 
     fun init() {
         defaultCloseOperation = EXIT_ON_CLOSE
         setSize(runtimeSettings.screenWidth, runtimeSettings.screenHeight)
         isResizable = runtimeSettings.isResizable
+        title = runtimeSettings.windowTitle
         isVisible = true
     }
 }
