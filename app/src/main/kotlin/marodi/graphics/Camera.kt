8330e654d13f8ef27a80816c871aee73b0c35c48
Matthew Dilley <matthewrdilley@gmail.com>
2024-04-01 22:23:36 -0400

    camera now only draws visuals that can be seen on the screen

--- app/src/main/kotlin/marodi/graphics/Camera.kt
+++ app/src/main/kotlin/marodi/graphics/Camera.kt
@@ -27,10 +27,19 @@ public class Camera internal constructor (
                 (-inY + y + screen.height).toInt()
     }
 
+    private fun isOnScreen(x: Float, y: Float, w: Float, h: Float): Boolean {
+        return toScreenX(x+w) >= 0 && toScreenX(x) <= screen.width
+                && toScreenY(y+h) >= 0 && toScreenY(y) <= screen.height
+    }
+
     public fun drawImage(img: BufferedImage?, x: Float, y: Float) {
-        val dx = toScreenX(x)
-        val dy = toScreenY(y) - img?.getHeight(null)!!
-        graphicsPanel.graphics2D?.drawImage(img, dx, dy, null)
+        if (img != null) {
+            if (isOnScreen(x, y, img.width.toFloat(), img.height.toFloat())) {
+                val dx = toScreenX(x)
+                val dy = toScreenY(y) - img.height
+                graphicsPanel.graphics2D?.drawImage(img, dx, dy, null)
+            }
+        }
     }
 
     public fun drawImage(img: BufferedImage?, positional: Positional) {
@@ -38,12 +47,14 @@ public class Camera internal constructor (
     }
 
     public fun drawRect(w: Float, h: Float, color: Color, x: Float, y: Float) {
-        graphicsPanel.graphics2D?.color = color
-        val ih = h.toInt()
-        val dx = toScreenX(x)
-        val dy = toScreenY(y) - ih
-        graphicsPanel.graphics2D?.fillRect(dx, dy, w.toInt(), ih)
-        graphicsPanel.graphics2D?.color = null
+        if (isOnScreen(x, y, w, h)) {
+            graphicsPanel.graphics2D?.color = color
+            val ih = h.toInt()
+            val dx = toScreenX(x)
+            val dy = toScreenY(y) - ih
+            graphicsPanel.graphics2D?.fillRect(dx, dy, w.toInt(), ih)
+            graphicsPanel.graphics2D?.color = null
+        }
     }
 
     public fun drawRect(w: Float, h: Float, color: Color, positional: Positional) {
