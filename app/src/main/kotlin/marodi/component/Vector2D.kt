7983b3aa0ba64e11cf6f37906779a19dda5ba39b
Matthew Dilley <matthewrdilley@gmail.com>
2024-08-03 19:32:41 -0400

    change method parameters from positional objects to x and y

--- app/src/main/kotlin/marodi/component/Vector2D.kt
+++ app/src/main/kotlin/marodi/component/Vector2D.kt
@@ -4,29 +4,27 @@ import java.util.*
 
 abstract class Vector2D : Vector<Positional>() {
 
-    public fun getAtPos(pos: Positional): Vector<Positional> {
+    public fun getAtPos(x: Float, y: Float): Vector<Positional> {
         val list = Vector<Positional>()
         for (o in this)
-            if (pos.x == o.x && pos.y == o.y) list.add(o)
+            if (x == o.x && y == o.y) list.add(o)
         return list
     }
 
-    public fun getInArea(pos1: Positional, pos2: Positional): Vector<Positional> {
-        var x1 = pos1.x
-        var y1 = pos1.y
-        var x2 = pos2.x
-        var y2 = pos2.y
+    public fun getInArea(x1_: Float, x2_: Float, y1_: Float, y2_: Float): Vector<Positional> {
+        var x1 = x1_
+        var x2 = x2_
+        var y1 = y1_
+        var y2 = y2_
 
         // Swaps values if in wrong order
         if (x1 > x2) {
-            x1 += x2
-            x2 = x1 - x2
-            x1 -= x2
+            x1 = x2_
+            x2 = x1_
         }
         if (y1 > y2) {
-            y1 += y2
-            y2 = y1 - y2
-            y1 -= y2
+            y1 = y2_
+            y2 = y1_
         }
 
         val list = Vector<Positional>()
