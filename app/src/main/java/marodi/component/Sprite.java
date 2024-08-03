b6da229433c729e059d5ea36e1c73be3106c9e7a
Matthew Dilley <matthewrdilley@gmail.com>
2024-08-03 19:20:29 -0400

    add depth field to Sprite class to determine in what order sprites are drawn

--- app/src/main/java/marodi/component/Sprite.java
+++ app/src/main/java/marodi/component/Sprite.java
@@ -6,6 +6,8 @@ import marodi.physics.PhysicalPositional;
 public abstract class Sprite extends PhysicalPositional implements Updatable, Drawable {
     private int lifespan;
     protected boolean visible = true;
+    protected float depth = 0; // higher depth means drawn before other sprites
+
     public void setVisible(boolean visible) {
         this.visible = visible;
     }
@@ -13,12 +15,16 @@ public abstract class Sprite extends PhysicalPositional implements Updatable, Dr
         return visible;
     }
     abstract public void start(Game game);
-
     public int getLifespan() {
         return lifespan;
     }
-
     public void increaseLifespan() {
         lifespan++;
+    }
+    public float getDepth() {
+        return depth;
+    }
+    public void setDepth(float depth) {
+        this.depth = depth;
     }
 }
--- app/src/main/kotlin/marodi/component/World.kt
+++ app/src/main/kotlin/marodi/component/World.kt
@@ -20,12 +20,23 @@ public open class World : Vector2D() {
     }
 
     fun draw(game: Game) {
+        val spritesToDraw = ArrayList<Sprite>()
         for (o in this)
             if (o is Sprite) {
                 if (o.isVisible)
-                    o.draw(game)
-            } else if (o is Drawable)
-                o.draw(game)
+                    spritesToDraw.add(o)
+            }
+        val orderedSpritesToDraw = ArrayList<Sprite>() // sprites in order from highest depth to lowest depth
+        while (spritesToDraw.isNotEmpty()) {
+            var highestDepthSprite = spritesToDraw[0]
+            for (sprite in spritesToDraw)
+                if (sprite.depth > highestDepthSprite.depth)
+                    highestDepthSprite = sprite
+            orderedSpritesToDraw.add(highestDepthSprite)
+            spritesToDraw.remove(highestDepthSprite)
+        }
+        for (sprite in orderedSpritesToDraw)
+            sprite.draw(game)
     }
 
     public open fun getGravity(game: Game): Float {
